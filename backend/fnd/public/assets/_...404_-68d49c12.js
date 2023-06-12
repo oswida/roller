845fcb94e67026a592cd1935ef7658444c586ce2
Oskar Świda <oskar.swida@gmail.com>
2023-06-12 09:43:14 +0200

    gitignore changes

--- backend/fnd/public/assets/_...404_-68d49c12.js
+++
@@ -1,1 +0,0 @@
-import{u as r,S as a,o as s,i as n,c as o,T as u,t as l}from"./index-3e48eaa4.js";function i(t){return r(a),s(()=>{}),null}const p=l("<main><h1>Page Not Found");function d(){return(()=>{const t=p(),e=t.firstChild;return n(t,o(u,{children:"Not Found"}),e),n(t,o(i,{code:404}),e),t})()}export{d as default};
