450fa9451fbbbb03c503c93a13b485d1fe618d38
Oskar Świda <oskar.swida@gmail.com>
2023-06-12 17:03:33 +0200

    fix: scrollbar style

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -10,6 +10,7 @@ import {
   Meta,
   Routes,
   Scripts,
+  Style,
   Title,
 } from "solid-start";
 import {
@@ -51,6 +52,31 @@ export default function Root() {
           href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&family=Oxanium:wght@400;700&family=Quattrocento:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        <Style>
+          {`input::-webkit-outer-spin-button,
+          input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+          }
+    
+          ::-webkit-scrollbar {
+            width: 8px;
+            height: 8px;
+          }
+    
+          ::-webkit-scrollbar-track {
+            -webkit-box-shadow: inset 0 0 6px rgba(86, 112, 145, 0.7);
+            border-radius: 10px;
+          }
+    
+          ::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            -webkit-box-shadow: inset 0 0 6px rgba(86, 112, 145, 0.9);
+          }
+    
+          ::-webkit-scrollbar-corner {
+          }`}
+        </Style>
       </Head>
       <Body>
         <Suspense>
